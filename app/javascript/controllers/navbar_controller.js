007b2a7af6482b34e6ec6b85a7069ba55747e5be
Daniel Lee <danlee1996@proton.me>
2023-12-17 11:04:14 -0700

    generated rails project, added bulma css, and navbar

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+	static targets = [ "navBurger", "navMenu" ]
+
+	connect() {}
+
+	toggleMobileMenu() {
+		this.navBurgerTarget.classList.toggle('is-active');
+		this.navMenuTarget.classList.toggle('is-active');
+	}
+}
