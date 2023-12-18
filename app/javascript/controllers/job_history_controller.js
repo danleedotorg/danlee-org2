5ff4ccc79869932f9ba2e23bbbb43f81bdde934f
Daniel Lee <danlee1996@proton.me>
2023-12-17 18:33:56 -0700

    added first job history UI view

--- app/javascript/controllers/job_history_controller.js
+++ app/javascript/controllers/job_history_controller.js
@@ -1,14 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-	static targets = ['description']
+	static targets = ['description', 'techUsed', 'techUsedButton', 'readButton']
 
-	connect() {
-		console.log(this.descriptionTarget)
-	}
+	connect() {}
 
 	toggleDescription() {
-		this.descriptionTarget.classList.toggle('is-hidden')
-	}
+		if (this.descriptionTarget.classList.toggle('is-hidden')) {
+			this.readButtonTarget.innerText = 'Read what I did'
+		} else {
+			this.readButtonTarget.innerText = 'Hide what I did'
+		}	}
 
+	toggleTechUsed() {
+		if (this.techUsedTarget.classList.toggle('is-hidden')) {
+			this.techUsedButtonTarget.innerText = 'Show tech used'
+		} else {
+			this.techUsedButtonTarget.innerText = 'Hide tech used'
+		}
+	}
 }
