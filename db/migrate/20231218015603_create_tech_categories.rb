b9d636a9a8972c16b3f961a20d99dacfbd780593
Daniel Lee <danlee1996@proton.me>
2023-12-17 19:16:37 -0700

    added null false to tech category migration

--- db/migrate/20231218015603_create_tech_categories.rb
+++ db/migrate/20231218015603_create_tech_categories.rb
@@ -1,7 +1,7 @@
 class CreateTechCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :tech_categories do |t|
-      t.text :name
+      t.text :name, null: false
 
       t.timestamps
     end
