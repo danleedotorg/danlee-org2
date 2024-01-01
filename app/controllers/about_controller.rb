867ad1f737fe7fb068d538e4429117d52f6619df
Daniel Lee <danlee1996@proton.me>
2024-01-01 16:16:48 -0700

    sorting job histories

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,5 @@
 class AboutController < ApplicationController
   def index
-    @job_histories = JobHistory.eager_load(:achievements).all
+    @job_histories = JobHistory.eager_load(:achievements).order(:start_date => :desc).all
   end
 end
